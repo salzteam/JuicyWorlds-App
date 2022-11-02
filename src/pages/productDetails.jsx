cce4c310c5f2104ef0b5ba5d50a58617c0c4712c
salzteam <akshalrizkigandari@gmail.com>
2022-11-02 16:06:06 +0700

    test

--- src/pages/productDetails.jsx
+++ src/pages/productDetails.jsx
@@ -94,16 +94,17 @@ class productDetails extends React.Component {
     const url = `${process.env.REACT_APP_BACKEND_HOST}/api/v1/products/${this.props.params.id}`
     Axios.get(url).then((res) => 
     this.setState({
-      id: res.data.data.dataProduct.id,
-      name: res.data.data.dataProduct.product_name,
-      price: res.data.data.dataProduct.price,
-      image: res.data.data.dataProduct.image,
-      desc: res.data.data.dataProduct.description,
-      ctg: res.data.data.dataProduct.category_name,
+      id: res.data["0"].id,
+      name: res.data["0"].product_name,
+      price: res.data["0"].price,
+      image: res.data["0"].image,
+      desc: res.data["0"].description,
+      ctg: res.data["0"].category_name,
       promo: res.data.data.dataPromo
     },() => {
       console.log(this.state.name)
-    })).catch((err) => console.log(err))
+    })
+    ).catch((err) => console.log(err))
   }
 
   setCtg = (category) => {
